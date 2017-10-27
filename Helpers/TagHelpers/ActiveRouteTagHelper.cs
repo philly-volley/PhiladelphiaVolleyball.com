a1fb781d85221eb938a06faca37731f3f47fbea8
danelliott <DanElliott@users.noreply.github.com>
2017-10-27 00:11:35 -0400

    added activeroute helper, favicon

---
+++ Helpers/TagHelpers/ActiveRouteTagHelper.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace PhiladelphiaVolleyball.com.Helpers.TagHelpers
+{
+    //Reference: https://gist.github.com/bjcull/4bdcc9d24e25cbac269e77dd93830ee3
+    [HtmlTargetElement(Attributes = "is-active-route")]
+    public class ActiveRouteTagHelper : TagHelper
+    {
+        private IDictionary<string, string> _routeValues;
+
+        /// <summary>The name of the action method.</summary>
+        /// <remarks>Must be <c>null</c> if <see cref="P:Microsoft.AspNetCore.Mvc.TagHelpers.AnchorTagHelper.Route" /> is non-<c>null</c>.</remarks>
+        [HtmlAttributeName("asp-action")]
+        public string Action { get; set; }
+
+        /// <summary>The name of the controller.</summary>
+        /// <remarks>Must be <c>null</c> if <see cref="P:Microsoft.AspNetCore.Mvc.TagHelpers.AnchorTagHelper.Route" /> is non-<c>null</c>.</remarks>
+        [HtmlAttributeName("asp-controller")]
+        public string Controller { get; set; }
+
+        /// <summary>Additional parameters for the route.</summary>
+        [HtmlAttributeName("asp-all-route-data", DictionaryAttributePrefix = "asp-route-")]
+        public IDictionary<string, string> RouteValues
+        {
+            get
+            {
+                if (this._routeValues == null)
+                    this._routeValues = (IDictionary<string, string>)new Dictionary<string, string>((IEqualityComparer<string>)StringComparer.OrdinalIgnoreCase);
+                return this._routeValues;
+            }
+            set
+            {
+                this._routeValues = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the <see cref="T:Microsoft.AspNetCore.Mvc.Rendering.ViewContext" /> for the current request.
+        /// </summary>
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public override async System.Threading.Tasks.Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            base.Process(context, output);
+
+            if (ShouldBeActive())
+            {
+                //if (context.TagName == "li")
+                //{
+                await MakeActiveAsync(output);
+                //}
+                //if (context.TagName == "a")
+                //{
+
+                //    MakeAccessible(output);
+                //}
+            }
+
+            output.Attributes.RemoveAll("is-active-route");
+        }
+
+        private bool ShouldBeActive()
+        {
+            string currentController = ViewContext.RouteData.Values["Controller"].ToString();
+            string currentAction = ViewContext.RouteData.Values["Action"].ToString();
+
+            if (!string.IsNullOrWhiteSpace(Controller) && Controller.ToLower() != currentController.ToLower())
+            {
+                return false;
+            }
+
+            if (!string.IsNullOrWhiteSpace(Action) && Action.ToLower() != currentAction.ToLower())
+            {
+                return false;
+            }
+
+            foreach (KeyValuePair<string, string> routeValue in RouteValues)
+            {
+                if (!ViewContext.RouteData.Values.ContainsKey(routeValue.Key) ||
+                    ViewContext.RouteData.Values[routeValue.Key].ToString() != routeValue.Value)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private async System.Threading.Tasks.Task MakeActiveAsync(TagHelperOutput output)
+        {
+            if (output.TagName == "a")
+            {
+                TagHelperContent content = await output.GetChildContentAsync();
+                output.Content.AppendHtml(content.GetContent() + " <span class=\"sr-only\">(current)</span>");
+            }
+            var classAttr = output.Attributes.FirstOrDefault(a => a.Name == "class");
+            if (classAttr == null)
+            {
+                classAttr = new TagHelperAttribute("class", "active");
+                output.Attributes.Add(classAttr);
+            }
+            else if (classAttr.Value == null || classAttr.Value.ToString().IndexOf("active", StringComparison.InvariantCultureIgnoreCase) < 0)
+            {
+                output.Attributes.SetAttribute("class", classAttr.Value == null
+                    ? "active"
+                    : classAttr.Value.ToString() + " active");
+            }
+        }
+
+        //private void MakeAccessible(TagHelperOutput output)
+        //{
+        //    output.Content.Append(this.Controller).AppendHtml(" <span class=\"sr-only\">(current)</span>");
+        //}
+    }
+}
